b2da485bb899223fb401eeae00af9b05506b2ec2
erratinsilentio <kacper.kondracki@wp.pl>
2025-05-05 08:07:31 +0200

    adding draft of a landing page, with hero section finished

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [new URL('https://cdn.dribbble.com/userupload/12302729/file/original-fa372845e394ee85bebe0389b9d86871.png?resize=1504x1128&vertical=center')],
+  },
 };
 
 export default nextConfig;
